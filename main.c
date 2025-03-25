0279554897619a663e63822aa3e36f109473abb7
Diilshodbek <odiljonovdilshodbek77@gmail.com>
2025-03-25 09:58:11 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
-#include  <stdio.h>
+int main(void) {
+    printf("Hello, World!\n");
 
-int main(void){
-    printf("Hello\n");
-
+    printf("%d\n", 10);
     return 0;
+
+    printf("%10d\n", 123);
+    printf("-%10d\n", 123);
+
+    int age = 25;
+    float pi = 3.14;
+    printf("Age: %d, PI: %.2f\n", age, pi);
 }
